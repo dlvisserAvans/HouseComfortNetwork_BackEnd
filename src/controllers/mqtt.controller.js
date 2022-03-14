bbd840124f386544ca0915382ae5baa3f82504e4
Dave Visser <dl.visser@student.protonmail.com>
2022-03-14 15:27:39 +0100

    chore: cleaned up the files

--- src/controllers/mqtt.controller.js
+++ src/controllers/mqtt.controller.js
@@ -1,12 +1,9 @@
 const assert = require('assert')
-// const logger = require('../config/config').logger
-// const movieDao = require('../dao/movie.dao')
-// const queries = require('../dao/queries')
 const mqtt = require('mqtt')
-const client = mqtt.connect({servers: [{host: 'dlv-lab.nl',port: 1883}], username : "dave", password: "Lyron_13"})
+const client = mqtt.connect({servers: [{host: 'dlv-lab.nl',port: 1883}], username : "dave", password: "T1rMbE&5Sjzlu62&9SSX?XBnYVVb2gxGYJ@0Fdks"})
 
 client.on('connect', function () {
-  client.subscribe('TestTopic', function (err) {
+  client.subscribe('ArduinoTopic', function (err) {
     if (!err) {
     //   client.publish('TestTopic', 'Hello mqtt')
       console.log("Successfully subscribed!")
@@ -14,16 +11,10 @@ client.on('connect', function () {
   })
 })
 
-client.on('message', function (topic, message) {
-  // message is Buffer
-  //console.log(message.toString())
-  //client.end()
-})
-
 // Generic function to handle results
 const handleResult = (res, next, err, result) => {
   if (err) {
-    logger.trace('getById', err.toString())
+    console.log('getById' + err.toString())
     next({
       errCode: 400,
       message: 'Failed calling query',
@@ -40,43 +31,9 @@ const handleResult = (res, next, err, result) => {
 }
 
 let controller = {
-//   /**
-//    * Check for the required itemproperties
-//    *
-//    * @param {*} req
-//    * @param {*} res
-//    * @param {*} next
-//    */
-//   validateMovie(req, res, next) {
-//     logger.info('validate movie')
-//     logger.info(req.body)
-//     try {
-//       const { name, releaseyear, studioid } = req.body
-//       assert(
-//         typeof name === 'string',
-//         "Property 'name' is missing from movieinfo!"
-//       )
-//       assert(
-//         typeof releaseyear === 'number',
-//         "Property 'releaseYear' is missing from movieinfo!"
-//       )
-//       assert(
-//         typeof studioid === 'number',
-//         "Property 'studioid' is missing from movieinfo!"
-//       )
-//       logger.trace('Movie data is valid')
-//       next()
-//     } catch (err) {
-//       logger.trace('Movie data is Invalid: ', err.message)
-//       res.status(400).json({
-//         message: 'Error!',
-//         error: err.message
-//       })
-//     }
-//   },
 
   /**
-   * Create a new movie
+   * Publish a new MQTT Message
    *
    * @param {*} req
    * @param {*} res
@@ -90,134 +47,11 @@ let controller = {
     console.log(topic)
     console.log(message)
     client.publish(topic, message)
-
-    if(message = undefined){
-        err.undefined
-    }
-    if(message != undefined){
-        result = "Successfully sent mqtt message!"
-    }   
+    let result = "MQTT Request sent"
+    let err = "MQTT Failed"
 
     handleResult(res, next, err, result)
   }
-
-//   /**
-//    * Geef alle movies, met optioneel query params.
-//    *
-//    * URL: localhost:3000/api/movies?name=Finding Nemo
-//    *
-//    * @param {*} req Incoming request object
-//    * @param {*} res Response to be returned
-//    * @param {*} next function to next route handler
-//    */
-//   getAll(req, res, next) {
-//     logger.info('getAll')
-
-//     let sqlQuery = queries.MOVIE_SELECT
-
-//     // We willen alle query params meenemen in de SQL query, door de key/values in de SQL
-//     // query toe te voegen. We kunnen de afzonderlijke keys als properties uitlezen, zoals:
-//     // const studioname = req.query.studio
-
-//     // Handiger/mooier:
-//     // Express levert req.query als een object met key/values, bv { name: 'Pixar', ...: ...}
-//     // Om te kunnen itereren maken we een array van het object met Object.entries
-//     const queryParams = Object.entries(req.query)
-//     // queryParmas is nu een array van arrays: [['name', 'Pixar'], ['...', ''...']]
-//     // Dit array kunnen we nu via arrayfuncties zoals map() en reduce() doorlopen.
-//     logger.info('queryParams:', queryParams)
-//     if (queryParams.length > 0) {
-//       let queryString = queryParams
-//         .map((param) => {
-//           // map maakt een nieuwe waarde van gegeven invoer; hier een string van twee arrayvalues.
-//           return `${param[0]} = '${param[1]}'`
-//         })
-//         .reduce((a, b) => {
-//           // reduce 'reduceert' twee opeenvolgende waarden tot één eindwaarde.
-//           return `${a} AND ${b}`
-//         })
-//       // logger.info('queryString:', queryString)
-//       sqlQuery += ` WHERE ${queryString};`
-//     }
-
-//     logger.debug('getAll', 'sqlQuery =', sqlQuery)
-
-//     movieDao.list(sqlQuery, (err, result) =>
-//       handleResult(res, next, err, result)
-//     )
-//   },
-
-//   /**
-//    * Geef 1 movie op basis van id
-//    *
-//    * @param {*} req Incoming request object
-//    * @param {*} res Response to be returned
-//    * @param {*} next function to next route handler
-//    */
-//   getById(req, res, next) {
-//     const movieId = req.params.movieId
-
-//     // logger.info('getById', movieId)
-//     console.log('getById Called!')
-
-//     // movieDao.getById(movieId, (err, result) =>
-//     //   handleResult(res, next, err, result)
-//     // )
-//     handleResult(res, next, null, "Hello there!" )
-//   },
-
-//   update(req, res, next) {
-//     // const userid = req.userId
-//     const id = req.params.movieId
-//     logger.debug('update', 'id =', id) //, 'userid =', userid)
-//     res.status(400).json({
-//       message: 'Not implemented yet!'
-//     })
-//   },
-
-//   deleteById(req, res, next) {
-//     const id = req.params.movieId
-//     const userid = req.userId
-//     logger.debug('delete', 'id =', id, 'userid =', userid)
-
-//     pool.getConnection(function (err, connection) {
-//       if (err) {
-//         res.status(400).json({
-//           message: 'delete failed!',
-//           error: err
-//         })
-//       }
-
-//       // Use the connection
-//       let sqlQuery = 'DELETE FROM movies WHERE id = ? AND UserID = ?'
-//       connection.query(sqlQuery, [id, userid], (error, results, fields) => {
-//         // When done with the connection, release it.
-//         connection.release()
-//         // Handle error after the release.
-//         if (error) {
-//           res.status(400).json({
-//             message: 'Could not delete item',
-//             error: error
-//           })
-//         }
-//         if (results) {
-//           if (results.affectedRows === 0) {
-//             logger.trace('item was NOT deleted')
-//             res.status(401).json({
-//               result: {
-//                 error: 'Item not found of you do not have access to this item'
-//               }
-//             })
-//           } else {
-//             logger.trace('item was deleted')
-//             res.status(200).json({
-//               result: 'successfully deleted item'
-//             })
-//           }
-//         }
-//       })
-//     })
-//   }
 }
 
 module.exports = controller
--- src/routes/test.route.js
+++ src/routes/test.route.js
@@ -1,27 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const testcontroller = require('../controllers/test.controller')
-//const authcontroller = require('../controllers/authentication.controller')
 
-// Movie routes
-// router.post(
-//   '',
-//   authcontroller.validateToken,
-//   testcontroller.validateMovie,
-//   testcontroller.createMovie
-// )
 router.get('', testcontroller.getById)
-// router.get('/:movieId', testcontroller.getById)
-// router.put(
-//   '/:movieId',
-//   authcontroller.validateToken,
-//   testcontroller.validateMovie,
-//   testcontroller.update
-// )
-// router.delete(
-//   '/:movieId',
-//   authcontroller.validateToken,
-//   testcontroller.deleteById
-// )
 
 module.exports = router
